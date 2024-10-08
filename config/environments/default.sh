8fec3538c8d83fbf49a28c25bdb9b5b6bc0046d9
Tonie Crumley <tonie.crumley@gmail.com>
2024-10-08 07:20:34 -0500

    Add a default environment configuration file
    The config/environments folder in a project will perform
    configuration specific to the researcher and the environment
    they work in.  A default environment file will be created by
    the framework and should be used to create an environment that
    will work anywhere with everything relative to the project
    directory.  Researchers can create environment configation files
    specific to them, their lab or their organization that will
    configure their local environment.  The LAB_ENV variable will
    specify which environment configuration should be used by the
    framework, e.g. LAB_ENV="lasseignelab".  The file should be named
    accordingly, e.g. lasseignelab.sh.  The LAB_ENV variable will
    usually be set in the ~/.labrc file.

---
+++ config/environments/default.sh
@@ -0,0 +1,4 @@
+# Default environment file to configure the base project where everything
+# is relative to the project folder.  This configuration should always
+# work for everyone attempting to reproduce this research without having
+# to make any changes.
